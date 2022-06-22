d153fc11438140230005e51c04e1dd82180dd8f5
Philipp Theyssen <p.theyssen@gmail.com>
2022-06-22 15:28:15 +0200

    Implement first benchmarks of inverses using criterion

---
+++ firststeps/src/Bench.hs
@@ -0,0 +1,21 @@
+module Bench where
+
+import Criterion.Main
+import Criterion.Types
+
+import Main hiding ( main )
+
+main :: IO ()
+main = do
+  putStrLn "Runnnnnnnnnnnning Bench.hs"
+  let xs = 0 : map (+5) xs :: [Int]
+  let ys = 10 : map (+10) ys :: [Int]
+  putStrLn "Benchmarking"
+  defaultMainWith
+    (defaultConfig {reportFile = Just "benchmarks.html"}) $
+    [bgroup "removeAt1" [ bench "4 [1,2,3,4,5,6]"
+                          $ nf (\x -> take 1 (removeAt1 4 x)) [1,2,3,4,5,6],
+                          bench "0 [3,2,1]"
+                          $ nf (\x -> take 1 (removeAt1 0 x)) [3,2,1]
+                        ]
+    ]
--- firststeps/src/Main.hs
+++ firststeps/src/Main.hs
@@ -7,18 +7,34 @@ import Plugin.InversionPlugin
 import Simulation
 import Compression
 import PartialInverses
+import Criterion.Main
+import Criterion.Types
 
-import Prelude hiding (map, lookup, (++), last, Maybe(..))
+import Prelude hiding (map, lookup, (++), last)
 
+
+-- main :: IO ()
+-- main = do
+--   putStr $ "Free fall simulation: " ++ (show fallDown) ++ "\n"
+--   putStr $ "First 5 inverses found : " ++ (show fallUp) ++ "\n"
+--   putStr $ "Compressed data : " ++ (show encoded) ++ "\n"
+--   putStr $ "Decompressed data : " ++ (show decoded) ++ "\n"
+--   putStr $ "removeAt1 3 [1,2,3,4,5] : " ++ (show removeAt1Example) ++ "\n"
 
 main :: IO ()
 main = do
-  putStr $ "Free fall simulation: " ++ (show fallDown) ++ "\n"
-  putStr $ "First 5 inverses found : " ++ (show fallUp) ++ "\n"
-  putStr $ "Compressed data : " ++ (show encoded) ++ "\n"
-  putStr $ "Decompressed data : " ++ (show decoded) ++ "\n"
-  putStr $ "removeAt1 3 [1,2,3,4,5] : " ++ (show removeAt1Example) ++ "\n"
-
+  putStrLn "Benchmarking automatically generated Inverses"
+  defaultMainWith
+    (defaultConfig {reportFile = Just "benchmarks.html"}) $
+    [bgroup "removeAt1" [ bench "4 [1,2,3,4,5,6]"
+                          $ nf (\x -> take 1 (removeAt1 4 x)) [1,2,3,4,5,6],
+                          bench "0 [3,2,1]"
+                          $ nf (\x -> take 1 (removeAt1 0 x)) [3,2,1]
+                        ],
+     bgroup "runLengthDecoder" [ bench "[1,5]"
+                                 $ nf (\x -> take 1 (runLengthDecoder x)) [1,5]
+                               ]
+    ]
 
 split :: [Int] -> [([Int], [Int])]
 split = $(inv '(++))
@@ -44,8 +60,13 @@ fallUp = take 5 $ freeFallInv fallDown
 -- Compressions
 --------------------------------------------------------------------------------
 
-runLengthEncoderInv :: [Int] -> [[Int]]
-runLengthEncoderInv = $(inv 'runLengthEncoder)
+runLengthDecoder :: [Int] -> [[Int]]
+runLengthDecoder = $(inv 'runLengthEncoder)
+
+
+-- TODO: implement for benchmarking
+runLengthDecoderManual :: [Int] -> [Int]
+runLengthDecoderManual = undefined
 
 dataToCompress :: [Int]
 dataToCompress = [1,1,1,1,1,1,1,2,2,3,3,3,5]
@@ -54,7 +75,7 @@ encoded :: [Int]
 encoded =  runLengthEncoder dataToCompress
 
 decoded :: [[Int]]
-decoded = take 1 $ runLengthEncoderInv encoded
+decoded = take 1 $ runLengthDecoder encoded
 
 --------------------------------------------------------------------------------
 -- Partial Inverses
@@ -63,6 +84,10 @@ decoded = take 1 $ runLengthEncoderInv encoded
 -- partial inverse of insertAt fixing the first argument (index)
 removeAt1 :: Int -> [Int] -> [(Int, [Int])]
 removeAt1 = $(partialInv 'insertAt [1])
+
+-- TODO: implement for benchmarking
+removeAt1Manual :: Int -> [Int] -> (Int, [Int])
+removeAt1Manual = undefined
 
 removeAt1Example :: [(Int, [Int])]
 removeAt1Example = (take 1 (removeAt1 3 [1,2,3,4,5]))
